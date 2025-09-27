ff25b3960f945a3faac74753037ac3a1d6b0ddf4
Scorp9514 <ertyu8571@gmail.com>
2025-09-27 16:44:31 +0800

    added folders

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const plusBtn = document.getElementById('plusBtn');
 const popup = document.getElementById('popup');
+const addWeb = document.getElementById('addWeb');
+const changeBg = document.getElementById('changeBg');
 
 plusBtn.addEventListener('click',()=>{
     const rect = plusBtn.getBoundingClientRect();
     popup.style.left = rect.left + "px";
     popup.style.right = rect.right + "px";
-    popup.style.display = popup.style.display === "block" ? "none" : "block";
+    popup.style.display = popup.style.display === "inline-block" ? "none" : "inline-block";
     console.log('yo')
 })
