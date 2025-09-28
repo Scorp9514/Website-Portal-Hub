2467073f515c246fe1c1bf70797d88dc2f1e9c96
Scorp9514 <ertyu8571@gmail.com>
2025-09-28 17:41:59 +0800

    got change background to work but not fetching uploading image rn

--- js/main.js
+++ js/main.js
@@ -2,11 +2,31 @@ const plusBtn = document.getElementById('plusBtn');
 const popup = document.getElementById('popup');
 const addWeb = document.getElementById('addWeb');
 const changeBg = document.getElementById('changeBg');
+const uploadBg = document.getElementById('uploadBg');
+const uploadBgText = document.getElementById('uploadBgText');
 
 plusBtn.addEventListener('click',()=>{
     const rect = plusBtn.getBoundingClientRect();
     popup.style.left = rect.left + "px";
-    popup.style.right = rect.right + "px";
+    // popup.style.top = rect.top + "px";
+    // if (popup.style.display === 'inline-block'){
+    //     const uploadBg = document.getElementById('uploadBg');
+    //     uploadBg.style.display = 'none';
+    //     console.log('a');
+    //     uploadBgText.display = 'none';
+    // }
+    
     popup.style.display = popup.style.display === "inline-block" ? "none" : "inline-block";
-    console.log('yo')
-})
+    const popupDisplay = window.getComputedStyle(popup).display;
+    // uploadBgText.display = popupDisplay;
+    uploadBgText.style.display = popup.style.display === 'none'?'none' : 'none';
+});
+
+changeBg.addEventListener('click',()=>{
+    const uploadBg = document.getElementById('uploadBg');
+    uploadBg.left = changeBg.left + "px";
+    uploadBg.top = changeBg.top + 'px';
+    // uploadBg.style.display = uploadBg.style.display === "inline-block" ? "none" : "inline-block";
+    uploadBgText.style.display = uploadBgText.style.display === 'inline-block' ? 'none' : 'inline-block';
+});
+
